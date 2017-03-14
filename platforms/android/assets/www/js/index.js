966b1362b2a8a3963d3e9d67aedbbd844c49ce47
Leandro Cesinaro <leandro.cesinaro@gmail.com>
2017-03-14 10:04:55 +0100

    Nuovo progetto

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -52,11 +52,18 @@ $(function(){
                        $.map(data,function(riga,indice){
                            console.log(indice);
                           $(contenuto).append('<div class="ui-corner-all custom-corners" data-id='+ indice +'><div class="ui-bar ui-bar-a"><h3>'+ riga.nome +'</h3><a class="ui-btn ui-btn-right ui-btn-icon-notext ui-icon-carat-r arrow-list"></a></div>\
-                                              <div class="ui-body ui-body-a custom-body"><img class="image-list" src='+ riga.immagine +' alt=""><p class="font">'+ riga.indirizzo +' - Tel '+ riga.telefono +'</p></div></div>');
+                                              <div class="ui-body ui-body-a custom-body"><a href="#"><img class="image-list" src='+ riga.immagine +' alt=""></a><p class="font">'+ riga.indirizzo +' - Tel '+ riga.telefono +'</p></div></div>');
                        });
                   })
                   .fail(function(){
                       alert("Errore!");
                   });
+    });
+    
+    $("div[data-role='page']").on("swiperight", function(event) {
+        $(this).find("div[data-role='panel']").panel("open");
+        });
+    $("div[data-role='page']").on("swipeleft", function(event) {
+        $(this).find("div[data-role='panel']").panel("close");
     });
 });
